67049b7f881b113647465a7e2a703d464b579228
tbanish <tombanish@gmail.com>
2020-07-17 18:48:55 -0400

    create seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+tom = User.create(username: "tbanish", email: "tom@tom.com", password: "test")
+james = User.create(username: "jbanish", email: "james@james.com", password: "test")
+nicole = User.create(username: "ngomez", email: , "nicole@nicole.com", password: "test")
+
+Book.create(title: "The Sun Also Rises", author: "Ernest Hemingway", user_id: tom.id)
+Book.create(title: "Blood Meridian", author: "Cormac McCarthy", user_id: tom.id)
+
+Book.create(title: "The Stand", author: "Stephen King", user_id: james.id)
+Book.create(title: "Catcher in the Rye", author: "JD Salinger", user_id: james.id)
+
+Book.create(title: "American Pastoral", author: "Philip Roth", user_id: nicole.id)
+Book.create(title: "Sharp Objects", author: "Gyllian Flynn", user_id: nicole.id)
--- notes.md
+++ notes.md
@@ -1,7 +1,7 @@
 Sprint 1.
 - [x] create users table
 - [x] create books table
-- [ ] create user model with associations
-- [ ] create book model with associations
+- [x] create user model with associations
+- [x] create book model with associations
 - [ ] create seed file with dummy data
 - [ ] seed data and play with models
