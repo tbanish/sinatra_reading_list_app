fa03dde348c6c59cb95a5c476ecab469925c1c78
tbanish <tombanish@gmail.com>
2020-07-17 19:51:22 -0400

    create routes for signup, login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < Sinatra::Base
   configure do
-    set :views, 'app/views'
-    set :public_folder, 'public'
+    set :views, "app/views"
+    set :public_dir, "public"
+    enable :sessions
+    set :session_secret, 'secret'
   end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/users' do
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+  end
+
+
 end
