b5381a77cd2d431f2642e9961d282aeda5a5dd17
tbanish <tombanish@gmail.com>
2020-07-17 18:50:14 -0400

    fix syntax error in seeds file and rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 tom = User.create(username: "tbanish", email: "tom@tom.com", password: "test")
 james = User.create(username: "jbanish", email: "james@james.com", password: "test")
-nicole = User.create(username: "ngomez", email: , "nicole@nicole.com", password: "test")
+nicole = User.create(username: "ngomez", email: "nicole@nicole.com", password: "test")
 
 Book.create(title: "The Sun Also Rises", author: "Ernest Hemingway", user_id: tom.id)
 Book.create(title: "Blood Meridian", author: "Cormac McCarthy", user_id: tom.id)
